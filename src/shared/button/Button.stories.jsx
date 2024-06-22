4f25151efb71244ef529f949ec7900b7be91fad8
Иван Лайер <ivan.layer@megafon.ru>
2024-06-22 20:09:19 +0300

    Своя кнопка в сторибук

---
+++ src/shared/button/Button.stories.jsx
@@ -0,0 +1,9 @@
+import {Button} from './Button';
+import './button.css';
+
+export default {
+    title: "UI/Button",
+    component: Button,
+}
+
+export const Default = () => <Button>Click me</Button> 
